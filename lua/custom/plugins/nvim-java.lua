e81cb3a5ce48d715e385430c9e47b8d690cb86d4
RunAtTekky <varunrawat343@gmail.com>
2026-01-14 14:07:46 +0530

    add: lombok path

--- lua/custom/plugins/nvim-java.lua
+++ lua/custom/plugins/nvim-java.lua
@@ -1,7 +1,16 @@
 return {
   'nvim-java/nvim-java',
   config = function()
-    require('java').setup()
+    local lombok_path = vim.fn.glob(vim.fn.expand '~/.gradle/caches/modules-2/files-2.1/org.projectlombok/lombok/1.18.20/*/lombok-1.18.20.jar')
+
+    require('java').setup {
+      jdtls = {
+        jvm_args = {
+          '-javaagent:' .. lombok_path,
+        },
+      },
+    }
+
     vim.lsp.enable 'jdtls'
   end,
 }
